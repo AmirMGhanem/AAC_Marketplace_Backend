fc4ca3af15f5658b77b5b9308bdb3fe6a2866b4e
Johann Thuillier <johann.t@adopt-a-contractor.com>
2023-03-05 17:33:31 +0200

    studio3t comment

--- app/core/config.py
+++ app/core/config.py
@@ -5,8 +5,10 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "AAC Marketplace"
     API_V1_STR: str = "/api/v1"
     #SQLALCHEMY_DATABASE_URI: str = "mysql+pymysql://root:@localhost/test"
+    # change config according to user
     SQLALCHEMY_DATABASE_URI: str = "mysql+pymysql://amir:Quc%N6FUA5E%40axlT1xD3fm@ps13.lmal.me/db_marketplace"
-
+    # TODO: to run mongo locally download studio3t
+    # MONGOALCHEMY_DATABASE_URI: str = "mongodb://localhost:27017/"
 
     class Config:
         env_file = ".env"
