0e8b73512680f9083ee152dacb9789f4866d3c3e
Amir M. Ghanem <amirwayne95@gmail.com>
2023-03-06 12:26:32 +0200

    utils/generate added for token. otp and .user apis needed

--- app/api/api_v1/crud/auth.py
+++ app/api/api_v1/crud/auth.py
@@ -1,11 +1,15 @@
-
 from sqlalchemy.orm import Session
 import hashlib
 from app.db.model_db import TUser
+from app.utils.generate import get_token
 
 
 def login(db: Session, user_login: str, user_password: str):
     hashed_user_password = hashlib.md5(user_password.encode()).hexdigest()
-    db_user = db.query(TUser).filter(TUser.user_login == user_login).filter(TUser.user_password == hashed_user_password).first()
-    return db_user
-
+    db_user = db.query(TUser).with_entities(TUser.user_login, TUser.user_name, TUser.user_company, TUser.user_role_id, TUser.user_id).filter(TUser.user_login == user_login).filter(TUser.user_password == hashed_user_password).first()
+    if db_user is not None:
+        db_user = db_user._asdict()
+        db_user = get_token(db_user)
+        return db_user
+    else:
+        return None
--- app/api/api_v1/crud/user.py
+++ app/api/api_v1/crud/user.py
@@ -1,9 +1,16 @@
 from sqlalchemy.orm import Session
-
+from app.utils.generate import decode_token
 from app.db.model_db import TUser
 
 def get_user(db: Session, user_id: int):
     return db.query(TUser).filter(TUser.user_id == user_id).first()
+
+
+def get_user_by_token(db: Session, token: str):
+    user= decode_token(token)
+    return db.query(TUser).filter(TUser.user_id == user['user_id']).first()
+
+
 
 def get_users(db: Session):
     return db.query(TUser).all()
